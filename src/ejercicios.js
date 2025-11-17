db7c7919522e2859646974c00703073f61f2af97
GiovanaTSW <giovana.diaz@tecdesoftware.edu.mx>
2025-11-16 22:02:47 -0600

    Ejercicio 1.3 resuelto

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -243,7 +243,26 @@ function matrizAImagen(matriz, rutaSalida) {
   //     };
   //   }
   // }
+function obtenerCanal(matriz, canal) {
+  if (!['r', 'g', 'b'].includes(canal)) {
+    throw new Error("El canal debe ser 'r', 'g', o 'b'");
+  }
 
+  const resultado = copiarMatriz(matriz);
+
+  for (let i = 0; i < resultado.length; i++) {
+    for (let j = 0; j < resultado[i].length; j++) {
+      const valor = matriz[i][j][canal];
+      resultado[i][j] = {
+        r: valor,
+        g: valor,
+        b: valor,
+        a: matriz[i][j].a
+      };
+    }
+  }
+  return resultado; // REEMPLAZAR CON TU CÓDIGO
+}
 
 /**
  * Ejercicio 1.4: Obtener dimensiones de una imagen (5 puntos)
